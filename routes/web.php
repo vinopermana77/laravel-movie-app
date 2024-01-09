4a24fee920f6d1cb729912c03005312eddc340c0
vino <it@kreditmandiri.co.id>
2024-01-09 10:42:44 +0700

    install spatie and test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('admin', function () {
+    return 'HI admin';
+})->middleware('role:admin');
+
+Route::get('user', function () {
+    return 'HI user';
+})->middleware('role:user');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
